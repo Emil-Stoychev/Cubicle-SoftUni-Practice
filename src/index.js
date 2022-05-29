92c09d205ad0591ad4945c2a44266428acbe829f
Емил Стойчев <estallinone@gmail.com>
2022-05-29 13:18:32 +0300

    Apply static files and modify views

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const port = 5000
 
 const app = express()
 
-app.use('/static', express.static('static'))
+app.use(express.static('./src/public'))
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
+app.set('views', './src/views')
+
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('index')
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`))
