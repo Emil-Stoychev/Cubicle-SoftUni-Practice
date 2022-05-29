276279ce58e4ab1a90feac8aaf341bb2ee910552
Емил Стойчев <estallinone@gmail.com>
2022-05-29 16:29:12 +0300

    Create works correct
    Make validation for create inputs and save new cube in the collection with unique id

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -1,7 +1,29 @@
 const router = require('express').Router()
+let cubes = require('../db.json')
+const fs = require('fs/promises')
+const path = require('path')
 
 router.get('/create', (req, res) => {
     res.render('create')
+})
+
+router.post('/create', (req, res) => {
+    let cube = req.body
+
+    if (!cube.name || !cube.description || !cube.imageUrl || !cube.difficultyLevel) {
+        if (cube.name == '' || cube.name.length == 0 || cube.description == '' || cube.description.length == 0 || cube.imageUrl == '' || cube.imageUrl.length == 0 || cube.difficultyLevel == '' || cube.difficultyLevel.length == 0) {
+            return res.status(400).send('Invalid request!')
+        }
+    }
+
+    let id = cubes[cubes.length - 1].id
+    cube.id = id + 1
+
+    cubes.push(cube)
+
+    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), { encoding: 'utf-8' })
+
+    res.redirect('/')
 })
 
 module.exports = router
